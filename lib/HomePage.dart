d57b2b4b3add672b3d284fc573c50763b267c49d
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2019-06-12 03:39:04 +0500

    Added text on menu bar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -18,9 +18,78 @@ class _HomeState extends State<Home> {
 
           new Container(
             margin: EdgeInsets.all(10.0),
-            height: 80.0,
+            height: 70.0,
             width: MediaQuery.of(context).size.width,
             color: Colors.indigo,
+            child: new Row(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: <Widget>[
+
+                new Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.all(10.0),
+                  decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.deepOrange,
+                  ),
+                  child: new Text("Home",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+
+                 new Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.all(10.0),
+                   decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.deepOrange,
+                  ),
+                  child: new Text("Blog",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+
+                 new Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.all(10.0),
+                   decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.deepOrange,
+                  ),
+                  child: new Text("New Post",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+
+                 new Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.all(10.0),
+                   decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.deepOrange,
+                  ),
+                  child: new Text("Registration",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+
+                 new Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.all(10.0),
+                   decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.deepOrange,
+                  ),
+                  child: new Text("Login",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+
+
+              ],
+            ),
 
           ),
 
